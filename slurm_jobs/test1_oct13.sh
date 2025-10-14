5cfe9e706dbd289f454931e470cd37efc187c46a
Mingi Kang <mkang2@bowdoin.edu>
2025-10-13 23:27:43 -0400

    [Test] Initial UNet and DnCNN tests

--- slurm_jobs/test1_oct13.sh
+++ slurm_jobs/test1_oct13.sh
@@ -14,7 +14,7 @@ cd /mnt/research/j.farias/mkang2/Convolutional-Nearest-Neighbor-Denoising
 
 source activate mingi
 
-# CBSD68 UNet 
+# # CBSD68 UNet 
 
 python main.py --model unet --layer Conv2d --channels 32 --num_pool_layers 4 --kernel_size 3 --padding 1 --dataset cbsd68 --data_path ./Data/CBSD68 --noise_std 0.1 --num_epochs 50 --seed 42 --padding 1 --lr_step 2 --lr_gamma 0.95 --lr 1e-2 --weight_decay 5e-4 --output_dir ./Output/Oct13_Denoising/CBSD/Unet_Conv2d_K3_S42
 
@@ -31,7 +31,7 @@ python main.py --model unet --layer ConvNN --channels 32 --num_pool_layers 4 --K
 python main.py --model unet --layer Branching --channels 32 --num_pool_layers 4 --kernel_size 3 --K 9 --sampling_type all --num_samples -1 --shuffle_pattern NA --shuffle_scale 0 --magnitude_type cosine --similarity_type Col --aggregation_type Col --padding 1 --dataset cifar10 --noise_std 0.1 --num_epochs 50 --seed 42 --padding 1 --lr_step 2 --lr_gamma 0.95 --lr 1e-2 --weight_decay 5e-4 --output_dir ./Output/Oct13_Denoising/CIFAR10/Unet_Branching_K9_S42
 
 
-# CBSD68 DnCNN 
+# # CBSD68 DnCNN 
 
 python main.py --model dncnn --layer Conv2d --channels 32 --num_layers 17 --kernel_size 3 --padding 1 --dataset cbsd68 --data_path ./Data/CBSD68 --noise_std 0.1 --num_epochs 50 --seed 42 --padding 1 --lr_step 2 --lr_gamma 0.95 --lr 1e-2 --weight_decay 5e-4 --output_dir ./Output/Oct13_Denoising/CBSD/DnCNN_Conv2d_K3_S42
 
